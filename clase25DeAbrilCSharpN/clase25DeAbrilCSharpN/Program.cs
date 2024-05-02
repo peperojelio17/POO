9b283806c80b64b6f85c42918d32dbf1e9dc56ab
Muriel <soiffermuriel@gmail.com>
2024-05-01 21:27:07 -0300

    recupero los archivos clase 25 de abril

---
+++ clase25DeAbrilCSharpN/clase25DeAbrilCSharpN/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace clase25DeAbrilCSharpN
+{
+
+    public class Copo
+    {
+        public int col = 0;
+        public int fila = 0;
+
+        public Copo(int col, int fila)
+        {
+            this.col = col;
+            this.fila = fila;
+        }
+    }
+    internal class Program
+    {
+
+        static TimeSpan transurso;
+        static DateTime h1 = DateTime.Now;
+        static DateTime h2 = DateTime.Now;
+        static int fila = 0;
+        static int columna = 0;
+        static List<Copo> copos = new List<Copo>();
+        static List<Copo> borrarCopos = new List<Copo>();
+
+        static int limite = 10;
+        static bool bajar(Copo copo)
+        {
+            bool total = true;
+            if (copo.fila == limite) 
+                return false;
+
+            foreach (Copo c in copos)
+            {
+                if (copo.fila + 1 == c.fila && copo.col == c.col)
+                {
+                    total = false;
+
+                }
+            }
+            return total;
+        }
+        static List<Copo> lleno()
+        {
+            int llenar = 0;
+
+            foreach (Copo copo in copos) { 
+                for (int i = 0; i < 8; i++)
+                {
+                    if (copo.col == i && copo.fila == limite)
+                    {
+                        llenar += 1;
+                    }
+                }
+            }
+
+            if (llenar== 8) {
+                for(int i = copos.Count;i > 0;i--)
+                {
+                    if (copos[i].fila == limite)
+                        borrarCopos.Add(copos[i]);
+                }
+            }
+            return borrarCopos;
+        }
+        static void Main(string[] args)
+        {
+            Copo a1;
+
+            Random r = new Random();
+            columna = r.Next(1, 8);
+            
+
+            for (int i = 0; i < 3; i++)
+            {
+                a1 = new Copo(r.Next(1, 8), 1);
+                copos.Add(a1);
+            }
+
+            while (true)
+            {
+                h2 = DateTime.Now;
+                transurso = h2 - h1;
+                Console.CursorVisible = false;
+
+                if (transurso.Milliseconds > 500)
+                {
+                    a1 = new Copo(r.Next(1, 8), 1);
+                    copos.Add(a1);
+                    foreach (Copo copo in copos)
+                    {
+
+                        if (bajar(copo)) { 
+                            Console.SetCursorPosition(copo.col, copo.fila);
+                            Console.Write(" ");
+                            copo.fila++;
+                            Console.SetCursorPosition(copo.col, copo.fila);
+                            Console.Write("*");
+                            foreach(Copo copito in lleno())
+                            {
+                                copos.Remove(copito);
+                            }
+                        } 
+                        h1 = h2;                       
+                        
+                    }
+
+                }
+                //for (int i = 0; i < 10; i++) {
+                //    Console.SetCursorPosition(10, 10 + i);
+                //    Console.Write("*");
+                //    Thread.Sleep(2000);
+                //    Console.Write(" ");
+                //}
+            }
+        }
+    }
+}
