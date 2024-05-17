0cd5b489793f2a41696c42e01c288688cc104255
Muriel <soiffermuriel@gmail.com>
2024-05-17 01:41:19 -0300

    le agregue funcionalidades al menu principal

--- menuPrincipal16_5/menuPrincipal16_5/Program.cs
+++ menuPrincipal16_5/menuPrincipal16_5/Program.cs
@@ -19,89 +19,155 @@ namespace Menu
     class MenuPrincipal
     {
         List<Menu> menu = new List<Menu>();
-        int posMenu = 1;
+        int posMenu = 0;
+        
         public MenuPrincipal(Dictionary<string, string[]> menus)
         {
             // Recorro el diccionario
-            int pos = 1;
+            int pos = 0;
+            int fila = 1;
+            int col = 1;
             foreach (var subMenu in menus)
             {
-                menu.Add(new Menu(pos++, subMenu.Key, subMenu.Value));
+                menu.Add(new Menu(pos, subMenu.Key, subMenu.Value, fila, col));
+                col += 30;
             }
         }
 
         public void derecha()
         {
             posMenu++;
+            if (posMenu + 1 > menu.Count)
+                posMenu = 0;
+            dibujar();
         }
         public void izquierda()
         {
             posMenu--;
+            if (posMenu < 0)
+                posMenu = menu.Count -1;
+            dibujar();
         }
 
 
         public void bajar()
         {            
-            menu[posMenu - 1].posItem++;
-            if (menu[posMenu - 1].posItem == menu[posMenu - 1].items.Length)
-                menu[posMenu - 1].posItem = 0;
+            menu[posMenu].posItem++;
+            if (menu[posMenu].posItem == menu[posMenu].items.Length)
+                menu[posMenu].posItem = 0;
+            dibujar();
+        }
+        public void subir()
+        {
+            menu[posMenu].posItem--;
+            if (menu[posMenu].posItem < 0)
+                menu[posMenu].posItem = menu[posMenu].items.Length - 1;
+            dibujar();
+        }
+        public void select()
+        {
+            Clientes p;
+            Console.Clear();
+            Console.SetCursorPosition(0,0);
+            Console.ForegroundColor = ConsoleColor.White;
+            switch (posMenu)
+            {
+                case 0:
+                    Console.WriteLine("-------Clientes-------");
+                    int dni,num = 0;
+                    string nombre;
+                    switch (menu[posMenu].posItem)
+                    {
+                        case 0:
+                            Console.WriteLine(menu[posMenu].items[0]);
+                            Console.Write("Ingrese su nombre:");
+                            nombre = Console.ReadLine();
+                            Console.Write("Ingrese su dni:");
+                            dni = int.Parse(Console.ReadLine());
+                            num++;
+                            p = new Clientes(num, nombre, dni);
+                            menu[posMenu].clientes.Add(p);
+                            Console.Write("Cliente ingresado");
+                            
+                            break;
+                        case 2:
+                            Console.WriteLine(menu[posMenu].items[2]);
+                            foreach(var cliente in menu[posMenu].clientes)
+                            Console.WriteLine(cliente.nombreCli);
+                            break;
+                    }
+                    nombre = Console.ReadLine();
+                    Console.Clear();
+                    dibujar();
+                    break;
+                case 1: 
+                    Console.WriteLine("-------Productos-------");
+                    nombre = Console.ReadLine();
+                    Console.Clear();
+                    dibujar();
+                    break;
+            }
         }
 
-        public void dibujar(int columna, int fila, int subMenu, int activo, int itemActivo)
+        public void dibujar()
         {
-            Console.SetCursorPosition(columna, fila++);
-            if (subMenu == activo) {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine(menu[subMenu - 1].nombreMenu);
-            }
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Black;
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine(menu[subMenu - 1].nombreMenu);
-            }
-            //foreach (string item in menu[subMenu - 1].items)
-            for (int i = 0; i < menu[subMenu - 1].items.Length; i++) 
-            {
-                Console.SetCursorPosition(columna, fila++);
-
-                if (subMenu == activo)
-                {
-                    if (menu[subMenu - 1].items[i] == menu[subMenu - 1].items[itemActivo])
+            
+            foreach (var can in menu) {
+                Console.SetCursorPosition(can.columna,can.fila++);
+                if (can == menu[posMenu]) {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(can.nombreMenu);
+                    for (int i = 0; i < can.items.Length; i++)
                     {
-                        Console.BackgroundColor = ConsoleColor.Red;
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine(menu[subMenu - 1].items[i]);
-                    }
-                    else
-                    {
-                        Console.BackgroundColor = ConsoleColor.Black;
-                        Console.ForegroundColor = ConsoleColor.White;
-                        Console.WriteLine(menu[subMenu - 1].items[i]);
+                        Console.SetCursorPosition(can.columna, can.fila++);
+                        if (can.items[i] == can.items[can.posItem])
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(can.items[i]);
+                        }
+                        else
+                        {
+                            Console.BackgroundColor = ConsoleColor.Black;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(can.items[i]);
+                        }
                     }
                 }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(can.nombreMenu);
+                    for (int i = 0; i < can.items.Length; i++)
+                    {
+                        Console.SetCursorPosition(can.columna, can.fila++);
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.WriteLine(can.items[i]);
+                    }
+                    }
+                    
+                
+                can.fila = 1;
             }
-            //bool primerItem = true;
-            //foreach (string item in items)
-            //{
-            //    if (primerItem)
-            //    {
-            //        Console.BackgroundColor = ConsoleColor.Red;
-            //        Console.ForegroundColor = ConsoleColor.White;
-            //        primerItem = false;
-            //    }
-            //    else
-            //    {
-            //        Console.BackgroundColor = ConsoleColor.Gray;
-            //        Console.ForegroundColor = ConsoleColor.Black;
-            //    }
-            //    Console.SetCursorPosition(columna, fila++);
-            //    Console.Write(item);
-            //}
+            
         }
     }
 
+    class Clientes
+    {
+        public int numCli;
+        public string nombreCli;
+        public int dni;
+        public Clientes(int numCli, string nombreCli, int dni)
+        {
+            this.numCli = numCli;
+            this.nombreCli = nombreCli;
+            this.dni = dni;
+        }
+    }
     class Menu
     {
         public string[] items;
@@ -109,12 +175,14 @@ namespace Menu
         public int posItem;
         public int columna;
         public int fila;
-
-        public Menu(int posMenu, string nombreMenu, string[] opciones)
+        public List<Clientes> clientes = new List<Clientes>();
+        public Menu(int posItem, string nombreMenu, string[] opciones, int fila, int col)
         {
             this.items = opciones;
             this.nombreMenu = nombreMenu;
-            this.posItem = posMenu;
+            this.posItem = posItem;
+            this.fila = fila;
+            this.columna = col;
         }
     }
 
@@ -123,8 +191,7 @@ namespace Menu
         static void Main(string[] args)
         {
             MenuPrincipal menu;
-            int activo = 1;
-            int itemActivo = 0;
+            
             int cantItem = 0;
             string[] menu1 = { "Nuevo Cliente", "Modificar Cliente", "Listar Clientes", "Salir" };
             string[] menu2 = { "Nuevo Producto", "Modificar Producto", "Eliminar Producto", "Listar Producto", "Salir" };
@@ -137,52 +204,26 @@ namespace Menu
 
             Console.Clear();
             menu = new MenuPrincipal(menus);
-            menu.dibujar(1, 1, 1, activo, itemActivo);
-            menu.dibujar(30, 1, 2, activo, itemActivo);
+            menu.dibujar();
 
             ConsoleKeyInfo tecla;
-             //Readkey devuelve un dato de tipo ConsoleKey
-            do {
+            //Readkey devuelve un dato de tipo ConsoleKey
+            do
+            {
                 tecla = Console.ReadKey();
                 if (tecla.Key == ConsoleKey.RightArrow)
-            {
-                Console.Clear();
-                if (activo > menus.Count -1) activo = 1;
-                else activo++;
-                menu.dibujar(1, 1, 1, activo, itemActivo);
-                menu.dibujar(30, 1, 2, activo, itemActivo);
-                    
-            }
+                    menu.derecha();
                 if (tecla.Key == ConsoleKey.LeftArrow)
-                {
-                    Console.Clear();
-                    if (activo == 1) activo = 2;
-                    else activo--;
-                    menu.dibujar(1, 1, 1, activo, itemActivo);
-                    menu.dibujar(30, 1, 2, activo, itemActivo);
-
-                }
-
-
-                menu.derecha();
+                    menu.izquierda();
                 if (tecla.Key == ConsoleKey.DownArrow)
-                {
-                    menu.bajar(); 
-                    menu.dibujar();                    
-                    //if (itemActivo > .Values.Count) activo = 1;
-                    //else itemActivo++;
-                    //menu.dibujar(1, 1, 1, activo, itemActivo);
-                    
-                }
+                    menu.bajar();
                 if (tecla.Key == ConsoleKey.UpArrow)
-                {
-                    menu.subir();                    
-                    menu.dibujar();                    
-                }
+                    menu.subir();
+                if (tecla.Key == ConsoleKey.Enter)
+                    menu.select();
+            } while (true);
 
-            } while(true);
-
-                Console.ReadKey();
+            Console.ReadKey();
 
         }
     }
