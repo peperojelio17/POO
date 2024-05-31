e7604dc51acb2af0d0dc805cc616ca4bdb7b5bcb
Muriel <soiffermuriel@gmail.com>
2024-05-31 00:22:31 -0300

    algunos cambios al tetris

---
+++ HerenciaTetris30_5/HerenciaTetris30_5/Pieza.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerenciaTetris30_5
+{
+    public abstract class Pieza
+    {
+        int limite = 25;
+        public List<Bloque> bloques = new List<Bloque>();
+
+        public Bloque a, b, c, d;
+
+        public bool bajar(List<Pieza> piezas)
+        {
+            Pieza piezaActual;
+            piezaActual = piezas.Last();
+            bool total = true;
+            foreach (var bloque in bloques)
+            {
+                if (bloque.x + 1 == limite)
+                {
+                    total = false;
+                    return total;
+                }
+            }
+            foreach (Pieza c in piezas)
+            {
+                if(c != piezaActual) { 
+                foreach (var bloque in bloques)
+                {
+                    foreach (var item in c.bloques)
+                    {
+                        if (bloque.x == item.x - 1 && bloque.y == item.y)
+                        {
+                            total = false;
+                        }
+                    }
+                }
+                }
+            }
+            return total;
+        }
+
+        public abstract void Rotar();
+        
+        public void derecha()
+        {
+            foreach (var bloque in bloques)
+            {
+                Console.SetCursorPosition(bloque.y + 30, bloque.x);
+                Console.Write(" ");
+                bloque.y++;
+            }
+
+        }
+        public void izquierda()
+        {
+            foreach (var bloque in bloques)
+            {
+                Console.SetCursorPosition(bloque.y + 30, bloque.x);
+                Console.Write(" ");
+                bloque.y--;
+            }
+        }
+        public void color()
+        {
+            Random r = new Random();
+            int color = r.Next(1,4);
+            switch (color)
+            {
+                case 1:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    break;
+                case 2:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+                case 3:
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    break;
+                case 4:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+            }
+                
+        }
+        public void nueva(List<Pieza> piezas)
+        {
+            Random r = new Random();
+            int num = r.Next(1, 6);
+            Linea a1;
+            Cubo a2;
+            Te a3;
+            Ele a4;
+            Ese a5;
+            Zeta a6;
+            switch (num)
+            {
+                case 1:
+                    a2 = new Cubo();
+                    piezas.Add(a2);
+                    break;
+                case 2:
+                    a3 = new Te();
+                    piezas.Add(a3);
+                    break;
+                case 3:
+                    a4 = new Ele();
+                    piezas.Add(a4);
+                    break;
+                case 4:
+                    a1 = new Linea();
+                    piezas.Add(a1);
+                    break;
+                case 5:
+                    a5 = new Ese();
+                    piezas.Add(a5);
+                    break;
+                case 6:
+                    a6 = new Zeta();
+                    piezas.Add(a6);
+                    break;
+            }
+        }
+    }
+}
---
+++ HerenciaTetris30_5/HerenciaTetris30_5/TodasLasPiezas.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerenciaTetris30_5
+{
+    public class Linea : Pieza
+    {
+        bool rotar = false;
+        public Linea()
+        {
+            a = new Bloque(1, 1);
+            b = new Bloque(1, 2);
+            c = new Bloque(1, 3);
+            d = new Bloque(1, 4);
+
+            bloques.Add(a);
+            bloques.Add(b);
+            bloques.Add(c);
+            bloques.Add(d);
+        }
+        public override void Rotar()
+        {
+            if (rotar== false) { 
+            Console.SetCursorPosition(a.y + 30, a.x);
+            Console.Write("    ");
+            a.y = b.y;  c.y = b.y;  d.y = b.y;
+            a.x = b.x-1;  c.x = b.x + 1;  d.x = b.x + 2;
+            rotar = true;
+            }
+            else
+            {
+                for (int i = 0; i < bloques.Count; i++) { 
+                Console.SetCursorPosition(a.y + 30, a.x + i);
+                Console.Write(" ");
+                }
+                a.x = b.x; c.x = b.x; d.x = b.x;
+                a.y = b.y - 1; c.y = b.y + 1; d.y = b.y + 2;
+                rotar = false;
+            }
+        }
+
+    }
+    public class Cubo : Pieza
+    {
+        public Cubo()
+        {
+            a = new Bloque(1, 1);
+            b = new Bloque(1, 2);
+            c = new Bloque(2, 1);
+            d = new Bloque(2, 2);
+            bloques.Add(a);
+            bloques.Add(b);
+            bloques.Add(c);
+            bloques.Add(d);
+        }
+        public override void Rotar()
+        {
+        }
+    }
+    public class Ele : Pieza
+    {
+        bool rotar = false;
+        public Ele()
+        {
+            a = new Bloque(1, 1);
+            b = new Bloque(1, 2);
+            c = new Bloque(1, 3);
+            d = new Bloque(2, 3);
+
+            bloques.Add(a);
+            bloques.Add(b);
+            bloques.Add(c);
+            bloques.Add(d);
+        }
+        public override void Rotar()
+        {
+
+            if (rotar == false)
+            {
+                Console.SetCursorPosition(a.y + 30, a.x);
+                Console.Write("   ");
+                Console.SetCursorPosition(c.y + 30, c.x +1);
+                Console.Write(" ");
+                a.y = b.y; c.y = b.y; d.y = b.y-1;
+                a.x = b.x - 1; c.x = b.x + 1; d.x = b.x + 1;
+                rotar = true;
+            }
+            else
+            {
+                for (int i = 0; i < bloques.Count - 1; i++)
+                {
+                    Console.SetCursorPosition(a.y + 30, a.x + i);
+                    Console.Write(" ");
+                }
+                Console.SetCursorPosition(a.y + 29, c.x);
+                Console.Write(" ");
+                a.x = b.x; c.x = b.x; d.x = b.x +1;
+                a.y = b.y - 1; c.y = b.y + 1; d.y = b.y + 1;
+                rotar = false;
+            }
+        }
+    }
+    public class Te : Pieza
+    {
+        bool rotar = false;
+        public Te()
+        {
+            a = new Bloque(1, 1);
+            b = new Bloque(1, 2);
+            c = new Bloque(1, 3);
+            d = new Bloque(2, 2);
+            bloques.Add(a);
+            bloques.Add(b);
+            bloques.Add(c);
+            bloques.Add(d);
+        }
+        public override void Rotar()
+        {
+
+            if (rotar == false)
+            {
+                Console.SetCursorPosition(a.y + 30, a.x);
+                Console.Write("   ");
+                Console.SetCursorPosition(b.y + 30, b.x + 1);
+                Console.Write(" ");
+                a.y = b.y; c.y = b.y; d.y = b.y - 1;
+                a.x = b.x - 1; c.x = b.x + 1; d.x = b.x;
+                rotar = true;
+            }
+            else
+            {
+                for (int i = 0; i < bloques.Count - 1; i++)
+                {
+                    Console.SetCursorPosition(a.y + 30, a.x + i);
+                    Console.Write(" ");
+                }
+                Console.SetCursorPosition(a.y + 28, b.x);
+                Console.Write(" ");
+                a.x = b.x; c.x = b.x; d.x = b.x + 1;
+                a.y = b.y - 1; c.y = b.y + 1; d.y = b.y;
+                rotar = false;
+            }
+        }
+    }
+    public class Ese : Pieza
+    {
+        public Ese()
+        {
+            a = new Bloque(1, 1);
+            b = new Bloque(1, 2);
+            c = new Bloque(2, 2);
+            d = new Bloque(2, 3);
+            bloques.Add(a);
+            bloques.Add(b);
+            bloques.Add(c);
+            bloques.Add(d);
+        }
+        public override void Rotar()
+        {
+
+            a = new Bloque(b.x - 1, b.y);
+            c = new Bloque(b.x + 1, b.y);
+            d = new Bloque(b.x + 2, b.y);
+        }
+    }
+    public class Zeta : Pieza
+    {
+        public Zeta()
+        {
+            a = new Bloque(1, 2);
+            b = new Bloque(1, 3);
+            c = new Bloque(2, 1);
+            d = new Bloque(2, 2);
+            bloques.Add(a);
+            bloques.Add(b);
+            bloques.Add(c);
+            bloques.Add(d);
+        }
+        public override void Rotar()
+        {
+
+            a = new Bloque(b.x - 1, b.y);
+            c = new Bloque(b.x + 1, b.y);
+            d = new Bloque(b.x + 2, b.y);
+        }
+    }
+}
