3b899257d2304cce1f78790a5167a0bb9ed3dfde
Muriel <soiffermuriel@gmail.com>
2024-08-09 14:50:55 -0300

    clase del 9 de agosto

---
+++ Interfaces08_09/Interfaces08_09/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces08_09
+{
+    interface IAves
+    {
+        string Name { get; }
+        void comer(int cantidad);
+    }
+    interface IVolar
+    {
+        string Name { get; }
+        void volar(int altura);
+    }
+    class Kiwi : IAves
+    {
+        public int cantidadComida = 0;
+        public string Name { get; set; }
+
+        public void comer(int cantidad)
+        {
+            cantidadComida += cantidad;
+        }
+    }
+    class Pajaro : IAves, IVolar
+    {
+        public string Name { get; set; }
+        public void comer(int cantidad)
+        {
+        }
+
+        public void volar(int altura)
+        {
+            
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
