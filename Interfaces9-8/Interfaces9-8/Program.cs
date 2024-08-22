ddb067a47e6d771d47332b23ca6d3884a78f5c4f
angela-baltierrez <angelabaltierrez@gmail.com>
2024-08-22 13:48:03 -0300

    clase interface 16/08

---
+++ Interfaces9-8/Interfaces9-8/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces9_8
+{
+
+    interface IAves
+    {
+        string Name { get; }
+        void comer(int cantidad);
+    }
+
+    interface Ivolar
+    {
+        void volar(int altura);
+    }
+
+    class Ñandu : IAves
+    {
+
+        public int cantidadComida = 0;
+
+        public string Name { get; }
+
+        public void comer(int cantidad)
+        {
+            cantidadComida += cantidad;
+        }
+    }
+
+    class Pajaro : IAves, Ivolar
+    {
+        public string Name { get; }
+
+        public void comer(int cantidad)
+        {
+     
+        }
+        public void volar(int alura)
+        {
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ñandu ñandu = new Ñandu();
+            ñandu.comer(102);
+            Console.WriteLine(ñandu.cantidadComida);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Interfaces9-8/InterfazGrafica15-8/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Menu;
+
+namespace InterfazGrafica15_8
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add(textBox1.Text);   
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show("Queres salir?","Para cerrar", MessageBoxButtons.YesNo);
+
+            if (resultado == DialogResult.No)
+            {
+                e.Cancel = true; 
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                listBox1.Items.Add(textBox1.Text);
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Add(textBox2.Text);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                listBox2.Items.Add(textBox2.Text);
+            }
+        }
+
+        private void listBox2_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            listBox1.Items.Add(listBox2.SelectedItem);
+            listBox2.Items.Remove(listBox2.SelectedItem);
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            listBox2.Items.Add(listBox1.SelectedItem);
+            listBox1.Items.Remove(listBox1.SelectedItem);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedItems.Count == 1)
+            {
+                listBox1.Items.Add(listBox2.SelectedItem);
+                listBox2.Items.Remove(listBox2.SelectedItem);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+                foreach (var item in listBox2.SelectedItems)
+                {
+                    listBox1.Items.Add(item);
+                }
+                for (int i = listBox2.SelectedItems.Count - 1; i >= 0 ; i--)
+                {
+                    listBox2.Items.RemoveAt(i);
+                }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count == 1)
+            {
+                listBox2.Items.Add(listBox1.SelectedItem);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            foreach (var item in listBox1.SelectedItems)
+            {
+                listBox2.Items.Add(item);
+            }
+            for (int i = listBox1.Items.Count - 1; i >= 0 ; i--)
+            {
+                listBox1.Items.RemoveAt(i);
+            }
+        }
+    }
+}
