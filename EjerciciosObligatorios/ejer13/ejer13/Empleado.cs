1f9d93579f0fe4a70560e00e688150e3ac3eba94
Muriel <soiffermuriel@gmail.com>
2024-09-27 15:01:09 -0300

    Empece con el ejer 13 obligatorio

---
+++ EjerciciosObligatorios/ejer13/ejer13/Empleado.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer13
+{
+    public abstract class Empleado
+    {
+        private string nombre;
+        private int edad;
+        private int salario;
+        private const int PLUS = 300;
+        public Empleado(string _nombre, int _edad, int _salario)
+        {
+            nombre = _nombre;
+            edad = _edad;
+            salario = _salario;
+        }
+        public string Nombre { get { return nombre; } }
+        public int Edad { get { return edad; } }
+        public int Salario { get { return salario; } set { salario = value; } }
+
+        public int Plus {get {return PLUS; } }
+
+        public abstract bool plus();
+
+
+    }
+    public class Repartidor : Empleado
+    {
+        private string zona;
+        public Repartidor(string nombre, int edad, int salario, string _zona) : base(nombre, edad, salario)
+        {
+            zona = _zona;
+        }
+        public string Zona { get { return zona; } }
+        public override bool plus()
+        {
+            if (base.Edad < 25 && zona == "zona 3")
+            {
+                base.Salario += base.Plus;
+                return true; 
+            }
+            return false;
+        }
+    }
+    public class Comercial : Empleado
+    {
+        private double comision;
+        public Comercial(string nombre, int edad, int salario, double _comision) : base(nombre, edad, salario)
+        {
+            comision = _comision;
+        }
+        public double Comision { get { return comision; } }
+
+        public override bool plus()
+        {
+            if(base.Edad > 30 && comision > 200)
+            {
+            base.Salario += base.Plus;
+            return true;
+            }
+        return false;
+        }
+    }
+}
---
+++ EjerciciosObligatorios/ejer13/ejer13/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comercial e1 = new Comercial("Rodrigo", 27, 2500, 310);
+            Repartidor e2 = new Repartidor("Gino", 28, 1400, "zona 1");
+            Comercial e3 = new Comercial("Marta", 40, 1200, 310);
+            Repartidor e4 = new Repartidor("Gustavo", 23, 1400, "zona 3");
+            Comercial e5 = new Comercial("Lucero", 20, 3100, 124);
+            Repartidor e6 = new Repartidor("Ana Maria", 18, 2030, "zona 2");
+            Comercial e7 = new Comercial("Ana Liza", 32, 2100, 240);
+            Repartidor e8 = new Repartidor("Yosua", 47, 1800, "zona 3");
+            Comercial e9 = new Comercial("Carla", 45, 4100, 180);
+
+            List<Empleado> list = new List<Empleado>() { e1,e2,e3,e4,e5,e6,e7,e8,e9};
+            string c = "";
+            Console.WriteLine("Datos de los empleados:");
+            foreach (Empleado e in list)
+            {
+                c = (e.GetType() == typeof(Comercial)) ? "Comercial": "Repartidor";
+                Console.WriteLine($"Nombre: {e.Nombre} --- Tipo: {c} --- Edad: {e.Edad} --- Salario: {e.Salario}");
+                
+            }
+            Console.WriteLine();
+            Console.WriteLine("Datos de los empleados tras analizar el plus:");
+            foreach (Empleado e in list)
+            {
+                if(e.plus()) Console.ForegroundColor = ConsoleColor.Green;
+                else Console.ForegroundColor = ConsoleColor.White;
+                c = (e.GetType() == typeof(Comercial)) ? "Comercial" : "Repartidor";
+                Console.WriteLine($"Nombre: {e.Nombre} --- Tipo: {c} --- Edad: {e.Edad} --- Salario: {e.Salario}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
