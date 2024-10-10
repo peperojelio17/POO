c7ad473d3e7018af3ffb7c47c21967e8183b9e74
Muriel <soiffermuriel@gmail.com>
2024-10-09 23:54:47 -0300

    avances con el ejer 17

---
+++ EjerciciosObligatorios/ejer17/ejer17/Baraja.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer17
+{
+    enum palosBarFrancesa
+    {
+        DIAMANTES,
+        PICAS,
+        CORAZONES,
+        TREBOLES
+    }
+    enum PalosBarEspañola
+    {
+        OROS,
+        COPAS,
+        ESPADAS,
+        BASTOS
+    }
+    public abstract class Baraja
+    {
+        public List<Carta> mazo;
+        private int numCartas;
+        private int numCartasPalo;
+        public Baraja(int _numCartas, int _numCartasPalo)
+        {
+            mazo = new List<Carta>();
+            numCartas = _numCartas;
+            numCartasPalo = _numCartasPalo;
+        }
+        public abstract void crearCartas();
+        
+    }
+    public class BarajaEspañola : Baraja
+    {
+        private bool jugar8y9;
+        public BarajaEspañola(int _numCartas, int _numCartasPalo) : base(_numCartas, _numCartasPalo)
+        {
+            jugar8y9 = false;
+        }
+
+        public override void crearCartas()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EjerciciosObligatorios/ejer17/ejer17/Carta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejer17
+{
+    public class Carta
+    {
+        static private Random r;
+        private char palo;
+        private int numero;
+
+        
+
+        public char Palo { get { return palo; } }
+        public int Numero { get { return numero; } }
+        public Carta()
+        {
+            r = new Random();
+            palo = elegirPalo();
+            numero = elegirNumero();
+        }
+        private char elegirPalo()
+        {
+            string palos = "EBOC";
+            char p = palos[r.Next(4)];
+            return p;
+        }
+        private int elegirNumero()
+        {
+            int num = 0;
+            while (num == 0 || num == 8 || num == 9)
+            {
+                num = r.Next(1, 13);
+            }
+            return num;
+        }
+    }
+}
